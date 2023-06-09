97c169f6073ae35b701a087fddc6ef4722af5676
aliammari <ali.ammari@esprit.tn>
2023-06-09 23:42:47 +0100

    live preview

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-    res.render('pad');
-})
-
+app.get("/", function (req, res) {
+  res.render("pad");
+});
 
 var port = process.env.PORT || 8000;
 app.listen(port);
